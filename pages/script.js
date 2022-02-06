6dc686c5475caef76f048ade7b5a12de3154c838
arulveena <arulpraveena05@gmail.com>
2022-02-06 04:02:50 -0800

    new changes

---
+++ pages/script.js
@@ -0,0 +1,53 @@
+function setNewImage()
+{
+    //Image.addEventListner('mouseenter',(container.innerText='welcome'));
+    
+    document.getElementById("img1").src="https://th.bing.com/th/id/OIP.j9Klu3DgyNjwwD-FhD4xzwHaEO?w=307&h=180&c=7&r=0&o=5&pid=1.7";
+   
+}
+function setOldImage()
+{
+    //Image.addEventListner('mouseleave',(container.innerText='welcome'));
+    document.getElementById("img1").src="https://res.cloudinary.com/innepal/image/upload/f_auto,q_auto/innepal/upload/image/learn-javascript-from-scratch.jpg";
+    
+}
+function setNewImage2()
+{
+    document.getElementById("img2").src="https://i.ytimg.com/vi/sgTLPWUNpgc/maxresdefault.jpg";
+}
+function setOldImage2()
+{
+    document.getElementById("img2").src="https://i.ytimg.com/vi/Uk6ifvL7VzY/maxresdefault.jpg";
+}
+function setNewImage3()
+{
+    document.getElementById("img3").src="https://i.ytimg.com/vi/uTrOK38bXnA/maxresdefault.jpg";
+}
+function setOldImage3()
+{
+    document.getElementById("img3").src="https://www.freecodecamp.org/news/content/images/2019/10/css.png";
+}
+function setNewImage4()
+{
+    document.getElementById("img4").src="https://cdn-images-1.medium.com/max/2000/1*SSutxOFoBUaUmgeNWAPeBA.jpeg";
+}
+function setOldImage4()
+{
+    document.getElementById("img4").src="https://www.game-designers.net/wp-content/uploads/2020/11/Learn-Python-Full-Course-for-Beginners-Tutorial.jpg";
+}
+function setNewImage5()
+{
+    document.getElementById("img5").src="https://th.bing.com/th/id/OIP.kaxj4hTWaNyzttydsDOF1gHaEK?pid=ImgDet&rs=1";
+}
+function setOldImage5()
+{
+    document.getElementById("img5").src="https://i.ytimg.com/vi/c4z-LkNea1k/maxresdefault.jpg";
+}
+function setNewImage6()
+{
+    document.getElementById("img6").src="https://i.ytimg.com/vi/wQu0wWfpTLY/maxresdefault.jpg";
+}
+function setOldImage6()
+{
+    document.getElementById("img6").src="https://i.ytimg.com/vi/heEtXxPQz7s/maxresdefault.jpg";
+}
